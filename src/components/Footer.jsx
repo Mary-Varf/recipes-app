70e571bef8546efb28666d7ad7c39e3154a9fd05
Mary-Varf <67269361+Mary-Varf@users.noreply.github.com>
2021-07-15 11:03:05 +0200

    added changes
    deleted connect link, fixed copyright block

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ function Footer () {
         <footer className="page-footer #b0bec5 blue-grey lighten-3">
           <div className="footer-copyright">
             <div className="container">
-            © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="https://github.com/Mary-Varf/react-shopl" target="_blank" rel="noreferrer">
+            © {new Date().getFullYear()} Maria Varfolomeeva
+            <a className="grey-text text-lighten-4 right" href="https://github.com/Mary-Varf/" target="_blank" rel="noreferrer">
               More Links</a>
             </div>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,10 @@ function Header() {
     return (
         <nav className='#546e7a blue-grey darken-1'>
         <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">React App</Link>
+          <Link to="/" className="brand-logo">Recipes App</Link>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contacts</Link>
             </li>
           </ul>
         </div>
