2b0a0bd1394cf6fe636117298ee3124e591744e8
Mary-Varf <67269361+Mary-Varf@users.noreply.github.com>
2021-07-15 11:01:23 +0200

    deleted basename and contacts li

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,18 @@ import {Footer} from './components/Footer';
 
 import {Home} from './pages/Home';
 import {About} from './pages/About';
-import {Contact} from './pages/Contact';
 import {NotFound} from './pages/NotFound';
 import {Category} from './pages/Category';
 import './index.css';
 import {Recipe} from './pages/Recipe';
 function App() {
     return <>
-        <Router basename='/recipes-app'>
+        <Router>
             <Header />
             <main className="container content">
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route path="/about" component={About}/>
-                    <Route path="/contact" component={Contact}/>
                     <Route path="/category/:name" component={Category}/>
                     <Route path="/meal/:id" component={Recipe}/>
                     <Route component={NotFound}/>
